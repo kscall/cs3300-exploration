a17dc0f570b2952db65eed2c525e4565a3305acf
Kim Call <kxmster@gmail.com>
2023-11-01 19:49:00 -0600

    First commit

---
+++ food_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class Review(models.Model):
+
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=200)
+    image = models.ImageField(required=False)
