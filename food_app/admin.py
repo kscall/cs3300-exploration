7b7ddd6754599447e2db41a0ba7be78b5c6142fe
Kim Call <kxmster@gmail.com>
2023-11-01 20:35:38 -0600

    Review Model (w/o stars)

--- food_app/admin.py
+++ food_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Review
+from star_ratings.models import Rating
 
-# Register your models here.
+# Register your models here so they can be edited in admin panel
+admin.site.register(Review)
--- food_app/models.py
+++ food_app/models.py
@@ -1,12 +1,23 @@
 from django.db import models
+from django.urls import reverse
+from star_ratings.models import Rating
 
 # Create your models here.
 
-'''
 class Review(models.Model):
 
-    name = models.CharField(max_length=200)
-    description = models.TextField(max_length=200)
-    image = models.ImageField(required=False)
+    name = models.CharField("Food", max_length=200)
+    details = models.TextField(max_length=200)
+    image = models.ImageField(blank=True)
+
+    #Define default String to return the name for representing the Model object."
+    def __str__(self):
+        return self.name
+
+
+    #Returns the URL to access a particular instance of MyModelName.
+    #if you define this method then Django will automatically
+    # add a "View on Site" button to the model's record editing screens in the Admin site
+    def get_absolute_url(self):
+        return reverse('review-detail', args=[str(self.id)])
     
-'''
